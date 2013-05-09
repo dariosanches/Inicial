177a8e63571653f8d66574ed7ec47b9c5a76c9e5
PC <PC@PC-HP>
2013-05-08 23:14:58 -0300

    Primeiro comit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: PC
+ *
+ * Created on 8 de Maio de 2013, 23:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+    printf("Primeiro projeto 08-05 mod");
+    return (EXIT_SUCCESS);
+}
+
